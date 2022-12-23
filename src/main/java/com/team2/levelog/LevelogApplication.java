a561d3048a4d6af4b9ba85135d63e42bb8a2836d
littlezero48 <littlezero.048@gmail.com>
2022-12-23 16:38:49 +0900

    기본 세팅

---
+++ src/main/java/com/team2/levelog/LevelogApplication.java
@@ -0,0 +1,13 @@
+package com.team2.levelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LevelogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team2/levelog/LevelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.levelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
