9503485a7527f560960e8ba2107513bdafbd0024
SHsus1122 <tjgurtn1122@naver.com>
2022-12-23 17:35:00 +0900

    FEAT: User Entity, Repository 생성 #1
    BUILD
    H2 DB 의존성 추가
    JWT Token 의존성 추가
    Validation 검증 의존성 추가
    MySQL 의존성 추가

--- src/main/java/com/team2/levelog/LevelogApplication.java
+++ src/main/java/com/team2/levelog/LevelogApplication.java
@@ -2,7 +2,9 @@ package com.team2.levelog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class LevelogApplication {
 
