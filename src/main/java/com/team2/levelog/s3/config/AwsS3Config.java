c1ad41fcdaea9d697125f1f93d782c6fbc6b42f1
syp0812 <94878158+syp0812@users.noreply.github.com>
2022-12-23 19:34:50 +0900

    Revert "#1 BUILD: S3 의존성 추가"
    This reverts commit e4125df1a7f723fbece88e101d57f7b55d0d6f56.

--- src/main/java/com/team2/levelog/s3/config/AwsS3Config.java
+++
@@ -1,7 +0,0 @@
-package com.team2.levelog.s3.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AwsS3Config {
-}
--- src/main/java/com/team2/levelog/s3/controller/AwsS3Controller.java
+++
@@ -1,9 +0,0 @@
-package com.team2.levelog.s3.controller;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequiredArgsConstructor
-public class AwsS3Controller {
-}
