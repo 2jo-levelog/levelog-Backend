e4125df1a7f723fbece88e101d57f7b55d0d6f56
syp0812 <94878158+syp0812@users.noreply.github.com>
2022-12-23 18:03:50 +0900

    #1 BUILD: S3 의존성 추가

---
+++ src/main/java/com/team2/levelog/s3/controller/AwsS3Controller.java
@@ -0,0 +1,9 @@
+package com.team2.levelog.s3.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AwsS3Controller {
+}
