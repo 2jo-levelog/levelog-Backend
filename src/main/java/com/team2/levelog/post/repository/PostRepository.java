a2e4cb9c2baafbf64264c1cbcad8023a2259d6d9
PigletHong <hongpiglet1995@gmail.com>
2022-12-27 13:33:06 +0900

    FEAT: 페이지 조회 시 페이징 기능 추가 (#2)

--- src/main/java/com/team2/levelog/post/repository/PostRepository.java
+++ src/main/java/com/team2/levelog/post/repository/PostRepository.java
@@ -1,6 +1,8 @@
 package com.team2.levelog.post.repository;
 
 import com.team2.levelog.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +11,9 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByOrderByModifiedAtDesc();   // 수정된 시간 기준
     List<Post> findAllByOrderByCreatedAtDesc();    // 생성된 시간 기준
     List<Post> findAllByUserId(Long id);
+    List<Post> findAllByUserEmail(String userEmail);
+//    List<Post> findAllByNickname(String userNickname);
+    Page<Post> findAllByNickname(String userNickname, Pageable pageable);
+
+//    Page<Post> findAllByNickname(String userNickname);
 }
